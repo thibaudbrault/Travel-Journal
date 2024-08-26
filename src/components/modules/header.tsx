73fe0eaeed2fc6d0b5ecc1aa9d54e4e0eddfcd48
Thibaud BRAULT <thibaud.brault@gide.net>
2024-08-26 16:55:34 +0200

    :construction: work on base for home page

---
+++ src/components/modules/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="px-8 py-4">
+        <h1 className="font-bold text-5xl text-purple-600">Travel Journal</h1>
+    </header>
+  );
+}
