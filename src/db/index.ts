ac9b5a7b95e41d49a88d587eca0abd8819b2e9a8
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-01 19:57:22 +0200

    :fire: remove unused config code

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,14 +3,5 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import postgres from "postgres";
 
-let sslMode = "require";
-if (process.env.NODE_ENV === "development") {
-  sslMode = "disable";
-}
-
-const pool = postgres(env.DATABASE_URL, {
-  ssl: sslMode === "disable" ? false : { rejectUnauthorized: false },
-});
-
-const pg = postgres(env.DATABASE_URL);
-export const db = drizzle(pg, { schema, logger: true });
+const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client, { schema });
