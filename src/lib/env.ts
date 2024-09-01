5fcd825c61754b26afb6caabacd2d80d9c248461
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-01 19:57:44 +0200

    :label: add google env variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  AUTH_GOOGLE_ID: z.string(),
+  AUTH_GOOGLE_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
