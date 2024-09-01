831feedd2926d9709760ec0cc8d7750a3c8c23a0
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-01 14:50:57 +0200

    :sparkles: add authjs

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+
+export const { handlers, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [Google],
+});
