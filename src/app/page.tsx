70712b2d2cc55efdd51cd20db076c3e34f555d20
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-04 09:28:44 +0200

    :bug: fix looping redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { auth } from "@/auth";
 import Home from "./home";
-import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const session = await auth();
-
-  if (!session?.user) redirect("/");
-
   return <Home />;
 }
