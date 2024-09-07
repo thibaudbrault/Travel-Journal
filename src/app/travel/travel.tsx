92a3270635e801d52ee48fa86bf89a8f29664c13
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-07 11:40:50 +0200

    :sparkles: add new fields

--- src/app/travel/travel.tsx
+++ src/app/travel/travel.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { createTravelSchema } from '@/actions/create-travel/schema';
 import { Button } from '@/components/ui/button';
 import {
   Form,
@@ -9,38 +10,42 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
 import { Textarea } from '@/components/ui/textarea';
 import { options } from '@/lib/constants';
 import { dateAtom, diffDaysAtom } from '@/lib/store';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useAtomValue } from 'jotai';
+import { redirect } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 type Props = {};
 
-const formSchema = z.object({
-  morning: z.string().min(2, {
-    message: 'You must add a descritption of your morning.',
-  }),
-});
+type Schema = z.infer<typeof createTravelSchema>;
 
 export default function Travel({}: Props) {
   const date = useAtomValue(dateAtom);
   const diffDays = useAtomValue(diffDaysAtom);
   const startDate = new Date(date?.from);
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  if (!date?.from) redirect('/new');
+
+  const form = useForm<Schema>({
+    resolver: zodResolver(createTravelSchema),
     defaultValues: {
+      breakfast: '',
       morning: '',
+      lunch: '',
+      afternoon: '',
+      diner: '',
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = (values: Schema) => {
     console.log(values);
-  }
+  };
 
   return (
     <main className="flex grow flex-col items-center justify-center gap-8 p-12">
@@ -60,38 +65,115 @@ export default function Travel({}: Props) {
           const curDate = new Date(date?.from);
           curDate.setDate(startDate.getDate() + index);
           return (
-            <section key={index} className="w-full">
-              <h2>
-                Day {index + 1}:{' '}
-                <span className="font-semibold capitalize">
-                  {curDate.toLocaleDateString(undefined, options)}
-                </span>
-              </h2>
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-4"
-                >
-                  <FormField
-                    control={form.control}
-                    name="morning"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Morning</FormLabel>
-                        <FormControl>
-                          <Textarea
-                            placeholder="Your morning activities"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit">Submit</Button>
-                </form>
-              </Form>
-            </section>
+            <>
+              <section key={index} className="w-full space-y-4">
+                <h2 className="text-xl">
+                  Day {index + 1}:{' '}
+                  <span className="font-semibold capitalize">
+                    {curDate.toLocaleDateString(undefined, options)}
+                  </span>
+                </h2>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4"
+                  >
+                    <fieldset className="space-y-2">
+                      <FormField
+                        control={form.control}
+                        name="breakfast"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Breakfast</FormLabel>
+                            <FormControl>
+                              <div className="flex gap-2 items-center">
+                                <Input
+                                  placeholder="Your breakfast"
+                                  {...field}
+                                />
+                                <Button variant="ghost">None</Button>
+                              </div>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="morning"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Morning</FormLabel>
+                            <FormControl>
+                              <Textarea
+                                placeholder="Your morning activities"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="lunch"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Lunch</FormLabel>
+                            <FormControl>
+                              <div className="flex gap-2 items-center">
+                                <Input
+                                  placeholder="Your breakfast"
+                                  {...field}
+                                />
+                                <Button variant="ghost">None</Button>
+                              </div>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="afternoon"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Afternoon</FormLabel>
+                            <FormControl>
+                              <Textarea
+                                placeholder="Your afternoon activities"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="diner"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Diner</FormLabel>
+                            <FormControl>
+                              <div className="flex gap-2 items-center">
+                                <Input placeholder="Your diner" {...field} />
+                                <Button variant="ghost">None</Button>
+                              </div>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </fieldset>
+                    <Button type="submit" className="font-semibold">
+                      Save
+                    </Button>
+                  </form>
+                </Form>
+              </section>
+              <Separator />
+            </>
           );
         })}
     </main>
