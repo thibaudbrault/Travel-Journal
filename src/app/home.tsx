f486b2b4484e5f41d647df2fd5f981e134b8dac7
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-08 19:54:48 +0200

    :fire: remove unused imports

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,4 @@
-import { signOutAction } from '@/actions/sign-out';
-import Link from 'next/link';
-
-type Props = {};
-
-export default async function Home({}: Props) {
+export default async function Home() {
   return (
     <main className="flex size-full flex-col items-center  justify-center gap-2 p-12 text-center">
       <h2 className="text-6xl font-semibold">
