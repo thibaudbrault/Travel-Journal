8554718874b6652269cb4a1bb86aa201635df4d7
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-08 19:56:24 +0200

    :bug: directly point to schema file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default {
-  schema: "./src/db/**",
-  out: "./migrations",
-  dialect: "postgresql",
+  schema: './src/db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
