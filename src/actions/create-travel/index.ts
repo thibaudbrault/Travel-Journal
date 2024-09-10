298c4a3031e4996848cdd40360c254eb23c2e2ab
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-10 17:55:42 +0200

    :recycle: return travelId to use in redirection

--- src/actions/create-travel/index.ts
+++ src/actions/create-travel/index.ts
@@ -1,27 +1,29 @@
 'use server';
 
-import { redirect } from 'next/navigation';
-
-import { Routes } from '@/lib/constants';
 import { authActionClient } from '@/lib/safe-actions';
 
 import { createTravelSchema } from './schema';
 
 import { db } from '@/db';
 import { travels } from '@/db/schema';
+import { redirect } from 'next/navigation';
+import { Routes } from '@/lib/constants';
 
 export const createTravel = authActionClient
   .metadata({ actionName: 'createTravel' })
   .schema(createTravelSchema)
   .action(
     async ({ parsedInput: { name, location, date }, ctx: { userId } }) => {
-      await db.insert(travels).values({
-        name,
-        location,
-        dateFrom: date.from,
-        dateTo: date.to,
-        userId,
-      });
-      redirect(Routes.TRAVEL);
+      const newTravel = await db
+        .insert(travels)
+        .values({
+          name,
+          location,
+          dateFrom: date.from,
+          dateTo: date.to,
+          userId,
+        })
+        .returning({ travelId: travels.id });
+      redirect(`${Routes.TRAVEL}?id=${newTravel[0].travelId}`);
     },
   );
