756ebf51058fa0a4c2799819b344ba2ce9aac91e
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-12 10:22:59 +0200

    :bug: replace location with country

--- src/actions/create-travel/index.ts
+++ src/actions/create-travel/index.ts
@@ -12,18 +12,16 @@ import { Routes } from '@/lib/constants';
 export const createTravel = authActionClient
   .metadata({ actionName: 'createTravel' })
   .schema(createTravelSchema)
-  .action(
-    async ({ parsedInput: { name, location, date }, ctx: { userId } }) => {
-      const newTravel = await db
-        .insert(travels)
-        .values({
-          name,
-          location,
-          dateFrom: date.from,
-          dateTo: date.to,
-          userId,
-        })
-        .returning({ travelId: travels.id });
-      redirect(`${Routes.TRAVEL}?id=${newTravel[0].travelId}`);
-    },
-  );
+  .action(async ({ parsedInput: { name, country, date }, ctx: { userId } }) => {
+    const newTravel = await db
+      .insert(travels)
+      .values({
+        name,
+        country,
+        dateFrom: date.from,
+        dateTo: date.to,
+        userId,
+      })
+      .returning({ travelId: travels.id });
+    redirect(`${Routes.TRAVEL}?id=${newTravel[0].travelId}`);
+  });
