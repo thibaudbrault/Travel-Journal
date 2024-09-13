7a9711ee6316c40e054ce5d90a56b8bcef1d0571
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-13 17:27:06 +0200

    :fire: remove throw error

--- src/actions/get-user/index.ts
+++ src/actions/get-user/index.ts
@@ -11,42 +11,33 @@ import { User, users } from '@/db/schema';
 import type { Session } from 'next-auth';
 
 export const getUser = cache(async (): Promise<User | null> => {
-  try {
-    const session = await auth();
-    const userId = session?.user?.id;
-    if (!userId) {
-      return null;
-    }
-    const user = await db.query.users.findFirst({
-      where: eq(users.id, userId),
-    });
-    if (!user) {
-      return null;
-    }
-    return user;
-  } catch {
-    throw new Error('Error fetching user');
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) {
+    return null;
   }
+  const user = await db.query.users.findFirst({
+    where: eq(users.id, userId),
+  });
+  if (!user) {
+    return null;
+  }
+  return user;
 });
 
 export const getUserId = cache(
   async (session: Session): Promise<string | null> => {
-    try {
-      const id = session?.user?.id;
-      if (!id) {
-        return null;
-      }
-      const user = await db.query.users.findFirst({
-        where: eq(users.id, id),
-      });
-      if (!user) {
-        return null;
-      }
-      const userId = user.id;
-      return userId;
-    } catch (error) {
-      console.log(error);
-      throw new Error('Error fetching user');
+    const id = session?.user?.id;
+    if (!id) {
+      return null;
     }
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, id),
+    });
+    if (!user) {
+      return null;
+    }
+    const userId = user.id;
+    return userId;
   },
 );
