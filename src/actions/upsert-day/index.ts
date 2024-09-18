f75baff424ce323024e76451dd788b1f90b9dcf6
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-18 17:21:26 +0200

    :bug: working upsert

--- src/actions/upsert-day/index.ts
+++ src/actions/upsert-day/index.ts
@@ -5,6 +5,9 @@ import { authActionClient } from '@/lib/safe-actions';
 import { db } from '@/db';
 import { days } from '@/db/schema';
 import { upsertDaySchema } from './schema';
+import { revalidatePath } from 'next/cache';
+import { Routes } from '@/lib/constants';
+import { sql } from 'drizzle-orm';
 
 export const upsertDay = authActionClient
   .metadata({ actionName: 'upsertDay' })
@@ -36,8 +39,16 @@ export const upsertDay = authActionClient
         .insert(days)
         .values(values)
         .onConflictDoUpdate({
-          target: days.id,
-          set: { ...values },
+          target: [days.date, days.travelId],
+          set: {
+            breakfast: sql`excluded.breakfast`,
+            morning: sql`excluded.morning`,
+            lunch: sql`excluded.lunch`,
+            afternoon: sql`excluded.afternoon`,
+            diner: sql`excluded.diner`,
+            link: sql`excluded.link`,
+          },
         });
+      revalidatePath(`${Routes.TRAVEL}/?id=${travelId}`);
     },
   );
